1659a2a7ad43bf7fafa687eb6ef4b091e907ca3a
Alex Johnson <alex.johnson@twinspires.com>
2017-08-23 11:52:21 -0400

    pipe demo

--- Angular-HelloWorld-master/src/app/app.component.ts
+++ Angular-HelloWorld-master/src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({ 
   selector: 'app-component',
   template: `
-    <h1>Hello World</h1>
+    <h1>{{ title | uppercase }}</h1>
+    <br><strong>Price:</strong> {{ price | currency:symbol:true }}
+    <br><strong>Date:</strong> {{ birthday | date:'medium' }}
   `
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+
+  title: string;
+  price: number;
+  symbol: string;
+  birthday: any;
+
   constructor() {
+
+  }
+
+  ngOnInit() {
+    this.title = 'Hello Lexington';
+    this.price = 3.50;
+    this.symbol = 'USD';
+    this.birthday = new Date();
 
   }
   
