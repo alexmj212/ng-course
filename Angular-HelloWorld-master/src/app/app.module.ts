a35883db4e091f3b51eb225d63518197974f2ec5
Alex Johnson <alex.johnson@twinspires.com>
2017-08-23 16:59:12 -0400

    two way data binding

--- Angular-HelloWorld-master/src/app/app.module.ts
+++ Angular-HelloWorld-master/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
@@ -7,7 +8,7 @@ import { GenericPipe } from './generic.pipe';
 import { ChildComponent } from "./child.component";
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, GenericPipe, ChildComponent ],
   bootstrap:    [ AppComponent ]
 })
--- Angular-HelloWorld-master/src/app/child.component.ts
+++ Angular-HelloWorld-master/src/app/child.component.ts
@@ -5,15 +5,29 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     template: `
         <h2>Child Header: {{title}}</h2>
         <br><button (click)="changeTitle()">Ask Chef a question</button>
+        <span *ngIf="emitted">Event emitted to parent!</span>
+        <br><h2>What kind of monster?</h2>
+            <input 
+                type="text" 
+                [(ngModel)]="monster">
+        <br><i>{{ monster }}</i>
         <br>   
     `
 })
 export class ChildComponent {
+
+    monster = "40ft tall monster from the Paleozoic era";
+    emitted = false;
 
     @Input() title: string;
     @Output() titleChanged = new EventEmitter<string>();
 
     changeTitle() {
         this.titleChanged.emit(this.title);
+        this.emitted = true;
+    }
+
+    changeMonster(value: string) {
+        this.monster = value;
     }
 }
